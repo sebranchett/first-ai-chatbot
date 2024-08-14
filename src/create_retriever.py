74dccae2332193b6164ed13a186e95d21f8ab8f4
sebranchett <s.e.branchett@tudelft.nl>
2024-08-14 12:17:55 +0200

    first email to DB

--- src/create_retriever.py
+++ src/create_retriever.py
@@ -3,14 +3,14 @@ from langchain.document_loaders.csv_loader import CSVLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 
-REVIEWS_CSV_PATH = "data/reviews.csv"
-REVIEWS_CHROMA_PATH = "chroma_data"
+EMAILS_CSV_PATH = "data/emails.csv"
+EMAILS_CHROMA_PATH = "chroma_data"
 
 dotenv.load_dotenv()
 
-loader = CSVLoader(file_path=REVIEWS_CSV_PATH, source_column="review")
-reviews = loader.load()
+loader = CSVLoader(file_path=EMAILS_CSV_PATH, source_column="Content")
+emails = loader.load()
 
-reviews_vector_db = Chroma.from_documents(
-    reviews, OpenAIEmbeddings(), persist_directory=REVIEWS_CHROMA_PATH
+emails_vector_db = Chroma.from_documents(
+    emails, OpenAIEmbeddings(), persist_directory=EMAILS_CHROMA_PATH
 )
