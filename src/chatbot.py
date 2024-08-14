81cfee238293399ccf5bcd0ee5d8f25b1c22924c
sebranchett <s.e.branchett@tudelft.nl>
2024-08-14 12:37:43 +0200

    adapted reviews to emails and changed context message

--- src/chatbot.py
+++ src/chatbot.py
@@ -11,13 +11,13 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.schema.runnable import RunnablePassthrough
 
-REVIEWS_CHROMA_PATH = "chroma_data/"
+EMAILS_CHROMA_PATH = "chroma_data/"
 
 dotenv.load_dotenv()
 
-review_template_str = """Your job is to use patient
-reviews to answer questions about their experience at
-a hospital. Use the following context to answer questions.
+email_template_str = """Your job is to use Erik's
+emails to answer questions about his life and work.
+Use the following context to answer questions.
 Be as detailed as possible, but don't make up any information
 that's not from the context. If you don't know an answer, say
 you don't know.
@@ -25,22 +25,22 @@ you don't know.
 {context}
 """
 
-review_system_prompt = SystemMessagePromptTemplate(
+email_system_prompt = SystemMessagePromptTemplate(
     prompt=PromptTemplate(
         input_variables=["context"],
-        template=review_template_str,
+        template=email_template_str,
     )
 )
 
-review_human_prompt = HumanMessagePromptTemplate(
+email_human_prompt = HumanMessagePromptTemplate(
     prompt=PromptTemplate(
         input_variables=["question"],
         template="{question}",
     )
 )
-messages = [review_system_prompt, review_human_prompt]
+messages = [email_system_prompt, email_human_prompt]
 
-review_prompt_template = ChatPromptTemplate(
+email_prompt_template = ChatPromptTemplate(
     input_variables=["context", "question"],
     messages=messages,
 )
@@ -49,16 +49,16 @@ chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
 output_parser = StrOutputParser()
 
-reviews_vector_db = Chroma(
-    persist_directory=REVIEWS_CHROMA_PATH,
+emails_vector_db = Chroma(
+    persist_directory=EMAILS_CHROMA_PATH,
     embedding_function=OpenAIEmbeddings()
 )
 
-reviews_retriever = reviews_vector_db.as_retriever(k=10)
+emails_retriever = emails_vector_db.as_retriever(k=10)
 
-review_chain = (
-    {"context": reviews_retriever, "question": RunnablePassthrough()}
-    | review_prompt_template
+email_chain = (
+    {"context": emails_retriever, "question": RunnablePassthrough()}
+    | email_prompt_template
     | chat_model
     | output_parser
 )
