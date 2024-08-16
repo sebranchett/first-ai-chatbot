3f926a573eea5e29e883b7fb6bbcfacd3f6accc9
sebranchett <s.e.branchett@tudelft.nl>
2024-08-16 15:24:02 +0200

    web interface works

---
+++ chatbot_frontend/src/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run any setup steps or pre-processing tasks here
+echo "Starting AllAboutErik chatbot frontend..."
+
+# Run the ETL script
+streamlit run main.py
---
+++ chatbot_frontend/src/main.py
@@ -0,0 +1,67 @@
+import os
+import requests
+import streamlit as st
+
+CHATBOT_URL = os.getenv("CHATBOT_URL", "http://localhost:8000/email-query")
+
+with st.sidebar:
+    st.header("About")
+    st.markdown(
+        """
+        This chatbot interfaces with a
+        [LangChain](https://python.langchain.com/docs/get_started/introduction)
+        agent designed to answer questions about Erik's weekly emails.
+        """
+    )
+
+    st.header("Example Questions")
+    st.markdown("- Does Erik have children?")
+    st.markdown("- What are important topics for Erik?")
+    st.markdown("- Who are Erik's colleagues?")
+
+st.title("All About Erik Chatbot")
+st.info(
+    "Ask me questions about topics that Erik covers in his weekly emails."
+)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        if "output" in message.keys():
+            st.markdown(message["output"])
+
+        if "explanation" in message.keys():
+            with st.status("How was this generated", state="complete"):
+                st.info(message["explanation"])
+
+if prompt := st.chat_input("What do you want to know?"):
+    st.chat_message("user").markdown(prompt)
+
+    st.session_state.messages.append({"role": "user", "output": prompt})
+
+    data = {"text": prompt}
+
+    with st.spinner("Searching for an answer..."):
+        response = requests.post(CHATBOT_URL, json=data)
+
+        if response.status_code == 200:
+            output_text = response.json()["output"]
+            explanation = response.json()["intermediate_steps"]
+
+        else:
+            output_text = """An error occurred while processing your message.
+            Please try again or rephrase your message."""
+            explanation = output_text
+
+    st.chat_message("assistant").markdown(output_text)
+    st.status("How was this generated", state="complete").info(explanation)
+
+    st.session_state.messages.append(
+        {
+            "role": "assistant",
+            "output": output_text,
+            "explanation": explanation,
+        }
+    )
